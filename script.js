dd7886286ccc37a1f458bd37dd1a2bab0ffbab59
Natada122 <bynatada122@gmail.com>
2019-08-19 11:39:27 +0300

    Правки

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function Person(name) {
   this.name = name;
-  Person.prototype.getName = function () {
-    return this.name;
-  };
 }
+
+Person.prototype.getName = function () {
+  return this.name;
+};
+
 function Man(name, facialHair) {
   Person.call(this, name);
   this.facialHair = facialHair;
-  Man.prototype.getName = () => `Name: ${Person.prototype.getName.call(this)}`;
-  Man.prototype.getFacialHair = () => this.facialHair;
-  Man.prototype = Object.create(Person.prototype);
-  Man.prototype.constuctor = Man;
 }
+
+Man.prototype.getName = () => `Name: ${Person.prototype.getName.call(this)}`;
+Man.prototype.getFacialHair = () => this.facialHair;
+Man.prototype = Object.create(Person.prototype);
+Man.prototype.constuctor = Man;
+
 const person = new Person('somebody');
 console.log(person.getName()); // somebody
 
